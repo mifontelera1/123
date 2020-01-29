55080f402b9a14815f9ac630535244c448da0a55
mifontelera1 <mifontelera1@gmail.com>
2020-01-30 05:02:52 +0800

    Updates

---
+++ precache-manifest.8593d148d52dfa844f839f9b39ca49ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fb488b4e05d89771d7ee22ef063ad8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88d4d886a86d49bd43de",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "33d24b09b08a7594edf1",
+    "url": "/static/js/2.e1419b37.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.e1419b37.chunk.js.LICENSE"
+  },
+  {
+    "revision": "88d4d886a86d49bd43de",
+    "url": "/static/js/main.37b881a9.chunk.js"
+  },
+  {
+    "revision": "ace97eb1fe5918b6d51f",
+    "url": "/static/js/runtime-main.f4a86112.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
